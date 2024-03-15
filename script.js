11141af40b163bd8e3fe626f587edeb47f9d23b3
Omar Ashour <68746440+OmarAshour02@users.noreply.github.com>
2024-03-15 23:46:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-let x = window.innerWidth / 2, y = window.innerHeight / 2;
+const SHAPE_WIDTH = 50, SHAPE_HEIGHT = 50;
+
+
+let mouseX = 1024, mouseY = 500;
 let ctx;
+let shapeX = mouseX, shapeY = mouseY;
+let mouseDown = false;
+
 
 const setup = () => {
     const cnv = document.getElementById('main-canvas');
@@ -7,35 +13,55 @@ const setup = () => {
     cnv.height = window.innerHeight;
 
     const dbgDev = document.getElementById("log");
-    dbgDev.innerHTML = `current X: ${x}, current Y: ${y}`;
+    dbgDev.innerHTML = `current X: ${mouseX}, current Y: ${mouseY}`;
 
     logMouseCoordinates();
 
-    window.addEventListener('mousemove', (e) => {
-        x = e.clientX;
-        y = e.clientY;
+    window.addEventListener('mousemove', (event) => {
+        mouseX = event.screenX;
+        mouseY = event.screenY;
+        logMouseCoordinates();
+    });
+
+    window.addEventListener('mousedown', (event) => {
+        if(checkMouseInbound()){
+            mouseDown = true;
+        }
+        logMouseCoordinates();
+    });
+
+    window.addEventListener('mouseup', (event) => {
+        mouseDown = false;
         logMouseCoordinates();
     });
 
     ctx = cnv.getContext("2d");
     ctx.fillStyle = "green";
-    ctx.fillRect(x, y, 50, 50);
+    ctx.fillRect(shapeX, shapeY, SHAPE_WIDTH, SHAPE_HEIGHT);
 }
 
 const logMouseCoordinates = () => {
     const dbgDev = document.getElementById("log");
-    dbgDev.innerHTML = `current X: ${x}, current Y: ${y}`;
+    dbgDev.innerHTML = `<b>MouseX:</b> ${mouseX}, <b>MouseY:</b> ${mouseY}`;
+    dbgDev.innerHTML += `<br> <b>ShapeX:</b> ${shapeX}, <b>ShapeY:</b> ${shapeY}`;
+    dbgDev.innerHTML += `<br> <b>MouseDown:</b> ${mouseDown}`;
+
 }
 
+
 const changePos = () => {
-    ctx.clearRect(x, y, 50, 50);
-    if(x == 450) {
-        y += 50;
-        x = 0;
+    ctx.clearRect(mouseX, mouseY, SHAPE_WIDTH, SHAPE_HEIGHT);
+    if(mouseX == 450) {
+        mouseY += 50;
+        mouseX = 0;
     } else {
-        x += 50;
+        mouseX += 50;
     }
-    ctx.fillRect(x, y, 50, 50);
+    ctx.fillRect(mouseX, mouseY, SHAPE_WIDTH, SHAPE_HEIGHT);
+}
+
+const checkMouseInbound = () => {
+    return true;
 }
 
 setup();
