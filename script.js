e6f113bccab6172dd349295cf1857fbb8d4b8076
Omar Ashour <omarashour02@gmail.com>
2024-03-16 01:27:17 +0200

    Store shapes in local storage
    - Changed different shapes colors
    - Made the local storage handle no more than 2 shapes
    - Stored the different shapes in same local storage

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const SHAPE_WIDTH = 200, SHAPE_HEIGHT = 200;
-
+const SHAPE_ID = Math.floor(Math.random() * 100);
 
 let mouseX = window.innerWidth / 2 - SHAPE_WIDTH / 2;
 let mouseY = window.innerHeight / 2 - SHAPE_HEIGHT / 2;
@@ -10,12 +10,10 @@ let mouseDown = false;
 
 const setup = () => {
     const cnv = document.getElementById('main-canvas');
+    let shapeColor = getShapeColor();
     cnv.width = window.innerWidth;
     cnv.height = window.innerHeight;
-
-
     logMouseCoordinates();
-
     window.addEventListener('mousemove', (event) => {
         mouseX = event.clientX - cnv.offsetLeft;
         mouseY = event.clientY - cnv.offsetTop;
@@ -38,16 +36,23 @@ const setup = () => {
         logMouseCoordinates();
     });
 
+    window.addEventListener('beforeunload', (event) => {
+        const shapes = JSON.parse(localStorage.getItem('shapes'));
+        delete shapes[SHAPE_ID];
+        localStorage.setItem('shapes', JSON.stringify(shapes));
+    });
+
     ctx = cnv.getContext("2d");
-    ctx.fillStyle = "green";
-    draw(shapeX, shapeY);
+    ctx.fillStyle = shapeColor;
+    draw(shapeX, shapeY, shapeColor);
 }
 
-const draw = (x, y) => {
+const draw = (x, y, color) => {
     ctx.clearRect(shapeX, shapeY, SHAPE_WIDTH, SHAPE_HEIGHT);
     shapeX = x;
     shapeY = y;
     ctx.fillRect(shapeX, shapeY, SHAPE_WIDTH, SHAPE_HEIGHT);
+    storeCanvasDimensions(color);
 }
 
 const logMouseCoordinates = () => {
@@ -62,5 +67,38 @@ const checkMouseInbound = () => {
     return mouseX >= shapeX && mouseX <= (shapeX + SHAPE_WIDTH) 
         && mouseY >= shapeY && mouseY <= (shapeY + SHAPE_HEIGHT);
 }
+
+const getShapeColor = () => {
+    if(localStorage.getItem("shapes") === null){
+        return "green";
+    }
+    
+    shapes = JSON.parse(localStorage.getItem("shapes"));
+    if(Object.keys(shapes).length == 2){
+        throw Error("Exceeded maximum shapes");
+    }
+
+    return shapes[Object.keys(shapes)[0]].color === "green" ? "blue" : "green";
+}
+
+function storeCanvasDimensions(shapeColor) {
+    let shapes = localStorage.getItem("shapes");
+    if(shapes == null) {
+        shapes = {};
+    } else {
+        shapes = JSON.parse(shapes);
+    }
+
+    const shape = {
+        id: SHAPE_ID,
+        x: shapeX, 
+        y: shapeY, 
+        color: shapeColor, 
+        state: mouseDown
+    };
+    shapes[SHAPE_ID] = shape;
+    localStorage.setItem('shapes', JSON.stringify(shapes));
+}
+
 
 setup();
