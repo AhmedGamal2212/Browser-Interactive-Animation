0d1dc3976f7aed9819cb89350883806a78290d5b
Ahmed Gamal <ahmedgamal.ssaleh@gmail.com>
2024-03-29 01:36:30 +0200

    Enable multiple shapes with random colors
    - remove number of shapes limit condition
    - change the getShapeColor function to return a random color
    - remove shapes object logging from the debugging function
    - change otherShape filteration logic to be based on the mouse state
    - change shape's dimensions to make it smaller
    - move circle radius to a constant at the begining of the script
    - reduce the sync interval to be five milliseconds instead of ten
    - log the number of shapes in the debugging function

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-const SHAPE_WIDTH = 200, SHAPE_HEIGHT = 200;
+const SHAPE_WIDTH = 100,
+    SHAPE_HEIGHT = 100;
+const CIRCLE_RADIUS = 5;
 const SHAPE_ID = crypto.randomUUID();
-const SYNC_INTERVAL = 10;
+const SYNC_INTERVAL = 5;
 
 let mouseX = window.innerWidth / 2 - SHAPE_WIDTH / 2;
 let mouseY = window.innerHeight / 2 - SHAPE_HEIGHT / 2;
 let deltaX, deltaY;
 let ctx;
-let shapeX = mouseX, shapeY = mouseY;
+let shapeX = mouseX,
+    shapeY = mouseY;
 let mouseDown = false;
 let counter = 1;
 
@@ -16,19 +19,19 @@ const setup = () => {
     let syncIntervalId;
     cnv.width = window.innerWidth;
     cnv.height = window.innerHeight;
-    deltaX = 0, deltaY = 0; 
+    (deltaX = 0), (deltaY = 0);
     logMouseCoordinates();
     window.addEventListener('mousemove', (event) => {
         mouseX = event.clientX - cnv.offsetLeft;
         mouseY = event.clientY - cnv.offsetTop;
         logMouseCoordinates();
-        if(mouseDown === true) {
+        if (mouseDown === true) {
             draw(mouseX - deltaX, mouseY - deltaY, shapeColor);
         }
     });
 
     window.addEventListener('mousedown', (event) => {
-        if(checkMouseInbound()){
+        if (checkMouseInbound()) {
             deltaX = Math.abs(mouseX - shapeX);
             deltaY = Math.abs(mouseY - shapeY);
             mouseDown = true;
@@ -50,14 +53,14 @@ const setup = () => {
         clearInterval(syncIntervalId);
     });
 
-    ctx = cnv.getContext("2d");
+    ctx = cnv.getContext('2d');
     ctx.fillStyle = shapeColor;
     draw(shapeX, shapeY, shapeColor);
 
     syncIntervalId = setInterval(() => {
         syncShapes(shapeColor);
     }, SYNC_INTERVAL);
-}
+};
 
 const draw = (x, y, color) => {
     const cnv = document.getElementById('main-canvas');
@@ -66,50 +69,46 @@ const draw = (x, y, color) => {
     shapeY = y;
     ctx.fillRect(shapeX, shapeY, SHAPE_WIDTH, SHAPE_HEIGHT);
     storeCanvasDimensions(color);
-}
+};
 
 const drawMouseHoldPosition = (x, y, squareShapeColor) => {
     ctx.beginPath();
-    ctx.arc(x, y, 10, 0, 2 * Math.PI, false);
-    ctx.fillStyle = 'red';
+    ctx.arc(x, y, CIRCLE_RADIUS, 0, 2 * Math.PI, false);
+    ctx.fillStyle = 'RED';
     ctx.fill();
     ctx.fillStyle = squareShapeColor;
-}
+};
 
 const logMouseCoordinates = () => {
-    const dbgDev = document.getElementById("log");
+    const dbgDev = document.getElementById('log');
+    const numberOfShapes = Object.keys(JSON.parse(localStorage.getItem('shapes') ? localStorage.getItem('shapes') : "[]")).length;
     dbgDev.innerHTML = `<b>MouseX:</b> ${mouseX}, <b>MouseY:</b> ${mouseY}`;
     dbgDev.innerHTML += `<br> <b>ShapeX:</b> ${shapeX}, <b>ShapeY:</b> ${shapeY}`;
     dbgDev.innerHTML += `<br> <b>MouseDown:</b> ${mouseDown}`;
     dbgDev.innerHTML += `<br> <b>DeltaX:</b> ${deltaX}, <b>DeltaY:</b> ${deltaY}`;
 
-    document.getElementById('shapes-log').innerHTML = `<br> <b>Shapes:</b> ${localStorage.getItem('shapes')}`;
-}
+    document.getElementById(
+        'shapes-log'
+    ).innerHTML = `<br> <b>Number of Shapes:</b> ${numberOfShapes} <br>`;
+    // + <br> <b>Shapes:</b> ${localStorage.getItem('shapes')}`;
+};
 
 const checkMouseInbound = () => {
-    return mouseX >= shapeX && mouseX <= (shapeX + SHAPE_WIDTH) 
-        && mouseY >= shapeY && mouseY <= (shapeY + SHAPE_HEIGHT);
-}
+    return (
+        mouseX >= shapeX &&
+        mouseX <= shapeX + SHAPE_WIDTH &&
+        mouseY >= shapeY &&
+        mouseY <= shapeY + SHAPE_HEIGHT
+    );
+};
 
 const getShapeColor = () => {
-    if(localStorage.getItem("shapes") === null){
-        return "green";
-    }
-    
-    const shapes = JSON.parse(localStorage.getItem("shapes"));
-    if(Object.keys(shapes).length === 0) {
-        return "green";
-    }
-    if(Object.keys(shapes).length == 2){
-        throw Error("Exceeded maximum shapes");
-    }
-
-    return shapes[Object.keys(shapes)[0]].color === "green" ? "blue" : "green";
-}
+    return '#' + Math.floor(Math.random() * 16777215).toString(16);
+};
 
 function storeCanvasDimensions(shapeColor) {
-    let shapes = localStorage.getItem("shapes");
-    if(shapes == null) {
+    let shapes = localStorage.getItem('shapes');
+    if (shapes == null) {
         shapes = {};
     } else {
         shapes = JSON.parse(shapes);
@@ -117,12 +116,12 @@ function storeCanvasDimensions(shapeColor) {
 
     const shape = {
         id: SHAPE_ID,
-        x: shapeX, 
-        y: shapeY, 
-        color: shapeColor, 
+        x: shapeX,
+        y: shapeY,
+        color: shapeColor,
         state: mouseDown,
-        mouseDeltaX : deltaX,
-        mouseDeltaY : deltaY
+        mouseDeltaX: deltaX,
+        mouseDeltaY: deltaY,
     };
 
     shapes[SHAPE_ID] = shape;
@@ -130,30 +129,36 @@ function storeCanvasDimensions(shapeColor) {
 }
 
 const syncShapes = (shapeColor) => {
-    if(mouseDown) {
+    if (mouseDown) {
         return;
     }
     const shapes = JSON.parse(localStorage.getItem('shapes'));
 
-    if(shapes === null) {
+    if (shapes === null) {
         return;
     }
     let otherShape;
-    for(const key of Object.keys(shapes)) {
-        if(key === SHAPE_ID) {
+    for (const key of Object.keys(shapes)) {
+        if (key === SHAPE_ID) {
             continue;
         }
-        otherShape = shapes[key];
+        if (shapes[key].state) {
+            otherShape = shapes[key];
+        }
     }
-    document.getElementById('session-count-log').innerHTML = `<br><b>Other Shape:</b> ${JSON.stringify(otherShape)}`;
-    if(otherShape === undefined || otherShape['state'] === false) {
+    document.getElementById(
+        'session-count-log'
+    ).innerHTML = `<br><b>Other Shape:</b> ${JSON.stringify(otherShape)}`;
+    if (otherShape === undefined || otherShape['state'] === false) {
         draw(shapeX, shapeY, shapeColor);
         return;
     }
     draw(otherShape['x'], otherShape['y'], shapeColor);
-    drawMouseHoldPosition(otherShape['x'] + otherShape['mouseDeltaX'], 
-            otherShape['y'] + otherShape['mouseDeltaY'], shapeColor);
-}
-
+    drawMouseHoldPosition(
+        otherShape['x'] + otherShape['mouseDeltaX'],
+        otherShape['y'] + otherShape['mouseDeltaY'],
+        shapeColor
+    );
+};
 
 setup();
